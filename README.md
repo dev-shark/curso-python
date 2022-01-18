17a5793a8c838ea0fbd0dd4d0a422f31400922e1
Adrian Devto <hgperu@gmail.com>
2022-01-18 02:13:54 -0500

    tipo de datos en python

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Curso de Python
+
+Curso de Python 8 horas -programación para todos-
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
---
+++ tipo_datos.py
@@ -0,0 +1,29 @@
+#Entero
+3
+#Float
+3.14
+#String
+"hola mundo"
+'hello world'
+#Boolean
+True
+False
+#Listas
+[1,2,3]
+[1,"2","tres"]
+#Tuplas
+(4,5,6)
+#Conjuntos
+{1,2,3,4}
+#Dictionary
+{1:"uno",2:"dos",3:"tres"}
+
+print(type(3))
+print(type(3.14))
+print(type("hola mundo"))
+print(type(True))
+print(type([1,2,3]))
+print(type((4,5,6)))
+print(type({1,2,3,4}))
+print(type({1:"uno",2:"dos",3:"tres"}))
+
